c2ef2e3c66f99cfd93507572245ac07b7dff826f
Shubham Patil <shubhamspatilnbr@gmail.com>
2024-07-13 19:20:35 +0530

    bk

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 3001;
 
 // CORS Middleware
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
